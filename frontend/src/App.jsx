ad5d4721892f31d3917742e6acaf0294dfea7f32
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-01-19 09:50:20 +0400

    update added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,9 @@ const App = () => {
   const [users, setUsers] = useState([]);
   const [state, setState] = useState({
     name: "",
-    age: undefined,
+    age: "",
   });
+  const [id, setId] = useState(undefined);
 
   const getData = async () => {
     const res = await axios.get("http://localhost:8080/users");
@@ -24,13 +25,25 @@ const App = () => {
 
   const addData = (e) => {
     e.preventDefault();
+
+    if (!state.name || !state.age) return;
+
     axios.post("http://localhost:8080/users", state);
     getData();
   };
 
-  const deleteData = (id) => {
-    console.log(id);
-    axios.delete(`http://localhost:8080/users/${id}`);
+  const deleteData = async (id) => {
+    await axios.delete(`http://localhost:8080/users/${id}`);
+    getData();
+  };
+
+  const handleEditClick = (data) => {
+    setState({ name: data.name, age: data.age });
+    setId(data.id);
+  };
+
+  const updateData = async (id) => {
+    await axios.put(`http://localhost:8080/users/${id}`, state);
     getData();
   };
 
@@ -40,26 +53,33 @@ const App = () => {
         <input
           name="name"
           type="text"
+          value={state.name}
           placeholder="name"
           onChange={handleChange}
         />
         <input
           name="age"
           type="text"
+          value={state.age}
           placeholder="age"
           onChange={handleChange}
         />
         <button>add</button>
       </form>
+      <button onClick={() => updateData(id)}>update</button>
 
       {users &&
-        users.map((user) => (
-          <li key={user.id}>
-            <span>{user.name}</span>
-            <span style={{ margin: 20 }}>{user.age}</span>
-            <button onClick={(id) => deleteData(user.id)}>delete</button>
-          </li>
-        ))}
+        users
+          .sort((a, b) => a.id - b.id)
+          .map((user) => (
+            <li key={user.id}>
+              <span>{user.id}</span>
+              <span>{user.name}</span>
+              <span style={{ margin: 20 }}>{user.age}</span>
+              <button onClick={(id) => deleteData(user.id)}>delete</button>
+              <button onClick={(id) => handleEditClick(user)}>edit</button>
+            </li>
+          ))}
     </>
   );
 };
